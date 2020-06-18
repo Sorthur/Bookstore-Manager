d4f3469006feeb84ce77e9730ff81f0a30a86cf1
Mateusz Dejewski <matdejewski@gmail.com>
2020-06-18 15:07:22 +0200

    New migration - price

---
+++ BookstoreManager/Migrations/202006181218291_AddedPricePropertyMigration.cs
@@ -0,0 +1,20 @@
+﻿namespace BookstoreManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedPricePropertyMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Books", "Price", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AddColumn("dbo.Orders", "Price", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Orders", "Price");
+            DropColumn("dbo.Books", "Price");
+        }
+    }
+}
