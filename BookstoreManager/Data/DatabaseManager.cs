707ef8b7434ff8df5e992cf5190714c1dc36f2b4
Mateusz Dejewski <matdejewski@gmail.com>
2020-07-02 20:48:08 +0200

    Added IDatabaseManager interface & its implementation

---
+++ BookstoreManager/Data/DatabaseManager.cs
@@ -0,0 +1,80 @@
+﻿using BookstoreManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BookstoreManager.Data
+{
+    public class DatabaseManager : IDatabaseManager
+    {
+        public async Task AddBookAsync(Book book)
+        {
+            using (var context = new DatabaseContext())
+            {
+                context.Books.Add(book);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteBookAsync(int id)
+        {
+            using (var context = new DatabaseContext())
+            {
+                //context.Books.Remove(book);
+                var book = await context.Books.FindAsync(id);
+                book.IsAvailable = false;
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task EditBookAsync(Book book)
+        {
+            using (var context = new DatabaseContext())
+            {
+                context.Books.AddOrUpdate(book);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Book> GetAvailableBookAsync(int id)
+        {
+            using (var context = new DatabaseContext())
+            {
+                return await context.Books.Where(b => b.IsAvailable == true).FirstOrDefaultAsync(b => b.Id == id);
+            }
+        }
+
+        public async Task<List<Book>> GetAvailableBooksAsync()
+        {
+            using (var context = new DatabaseContext())
+            {
+                return await context.Books.Where(b => b.IsAvailable == true).ToListAsync();
+            }
+        }
+
+        public async Task<List<Order>> GetOrdersAsync()
+        {
+            using (var context = new DatabaseContext())
+            {
+                var orders = await context.Orders.ToListAsync();
+                orders = await context.Orders
+                    .Include(b => b.OrderedBook)
+                    .ToListAsync();
+                return orders;
+            }
+        }
+
+        public async Task AddOrder(Order order)
+        {
+            using (var context = new DatabaseContext())
+            {
+                context.Orders.Add(order);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ BookstoreManager/Data/IDatabaseManager.cs
@@ -0,0 +1,21 @@
+﻿using BookstoreManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreManager.Data
+{
+    public interface IDatabaseManager
+    {
+        Task<List<Book>> GetAvailableBooksAsync();
+        Task<Book> GetAvailableBookAsync(int id);
+        Task AddBookAsync(Book book);
+        Task EditBookAsync(Book book);
+        Task DeleteBookAsync(int id);
+        Task AddOrder(Order order);
+        Task<List<Order>> GetOrdersAsync();
+
+    }
+}
